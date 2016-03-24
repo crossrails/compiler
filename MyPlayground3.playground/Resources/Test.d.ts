8cb23fe178ef6472592f47a7ce9f9c870c0c281d
Nick Bransby-Williams <nick@laundrapp.com>
2016-03-24 22:35:02 +0000

    add complier project

--- MyPlayground3.playground/Resources/Test.d.ts
+++
@@ -1,7 +0,0 @@
-declare let stringVar: string;
-declare const stringConst: string;
-declare let booleanVar: boolean;
-declare var undefinedBoolVar: boolean;
-declare let numberVar: number;
-declare let numberArrayVar: number[];
-declare let anyVar: any;
