0d1179912824a9414ca1d18395de02d1cbbc6ee3
Nick Bransby-Williams <nick@laundrapp.com>
2016-03-31 19:37:05 +0100

    switch to acorn

--- src/main.js
+++ src/main.js
@@ -1,70 +1,8 @@
 "use strict";
-var ts = require("typescript");
-var fs = require("fs");
-;
-/** Generate documention for all classes in a set of .ts files */
-function generateDocumentation(fileNames, options) {
-    // Build a program using the set of root file names in fileNames
-    var program = ts.createProgram(fileNames, options);
-    // Get the checker, we will use it to find more about classes
-    var checker = program.getTypeChecker();
-    var output = [];
-    // Visit every sourceFile in the program    
-    for (var _i = 0, _a = program.getSourceFiles(); _i < _a.length; _i++) {
-        var sourceFile = _a[_i];
-        // Walk the tree to search for classes
-        ts.forEachChild(sourceFile, visit);
-    }
-    // print out the doc
-    fs.writeFileSync("classes.json", JSON.stringify(output, [], 4));
-    return;
-    /** visit nodes finding exported classes */
-    function visit(node) {
-        // Only consider exported nodes
-        if (!isNodeExported(node)) {
-            return;
-        }
-        console.log(node.getText());
-        if (node.kind === ts.SyntaxKind.ClassDeclaration) {
-            // This is a top level class, get its symbol
-            var symbol = checker.getSymbolAtLocation(node);
-            output.push(serializeClass(symbol));
-        }
-        else if (node.kind === ts.SyntaxKind.ModuleDeclaration) {
-            // This is a namespace, visit its children
-            ts.forEachChild(node, visit);
-        }
-    }
-    /** Serialize a symbol into a json object */
-    function serializeSymbol(symbol) {
-        return {
-            name: symbol.getName(),
-            documentation: ts.displayPartsToString(symbol.getDocumentationComment()),
-            type: checker.typeToString(checker.getTypeOfSymbolAtLocation(symbol, symbol.valueDeclaration))
-        };
-    }
-    /** Serialize a class symbol infomration */
-    function serializeClass(symbol) {
-        var details = serializeSymbol(symbol);
-        // Get the construct signatures
-        var constructorType = checker.getTypeOfSymbolAtLocation(symbol, symbol.valueDeclaration);
-        details.constructors = constructorType.getConstructSignatures().map(serializeSignature);
-        return details;
-    }
-    /** Serialize a signature (call or construct) */
-    function serializeSignature(signature) {
-        return {
-            paramters: signature.parameters.map(serializeSymbol),
-            returnType: checker.typeToString(signature.getReturnType()),
-            documentation: ts.displayPartsToString(signature.getDocumentationComment())
-        };
-    }
-    /** True if this is visible outside this file, false otherwise */
-    function isNodeExported(node) {
-        return (node.flags & ts.NodeFlags.Export) !== 0 || (node.parent && node.parent.kind === ts.SyntaxKind.SourceFile);
-    }
-}
-generateDocumentation(process.argv.slice(2), {
-    target: ts.ScriptTarget.ES6, module: ts.ModuleKind.CommonJS
-});
+/// <reference path="../typings/main/ambient/acorn/index.d.ts" />
+/// <reference path="../typings/main/ambient/node/index.d.ts" />
+var fs_1 = require('fs');
+var acorn = require('acorn');
+console.log(acorn.parse(fs_1.readFileSync(process.argv[2]).toString()));
+console.log("None");
 //# sourceMappingURL=main.js.map
