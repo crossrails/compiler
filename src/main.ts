a095a9b24b7122aa47e5cd2523c45ae104d55ca5
Nick Williams <nbransby@gmaill.com>
2016-06-04 15:09:00 +0100

    get main tests passing

--- src/main.ts
+++ src/main.ts
@@ -2,94 +2,104 @@ import {log} from "./log"
 import {Module} from "./ast"
 import {Compiler, CompilerOptions} from "./compiler"
 
-import args = require('yargs');
+import yargs = require('yargs');
 
-let options: CompilerOptions & {logLevel: string, charset: string} = args
-    .usage('Usage: $0 [file.js] [options]')
-    .demand(1)
-    .check((argv: args.Argv, aliases: { [key: string]: string[] }) => {
-        if(argv._[0] && !argv._[0].endsWith('.js')) throw 'File argument must be a js source file (.js)';
-        return true;
-    })
-    .example('$0 src.min.js --swift', 'Compile to swift, outputting beside original source files')
-    .example('$0 src.js --java.outDir java', 'Compile to java, outputting to a java subdirectory')
-    .alias('v', 'version').version()
-    .help('h').alias('h', 'help')
-    .group(['p', 'l', 'h', 'v'], 'Global options:')
-    .option('p', {
-        config: true,
-        alias: 'project',
-        describe: 'Path to a xrails.json project config file (or to a directory containing one)',
-        type: 'string'
-    })
-    .option('charset', {
-        default: 'utf8',
-        describe: 'The character set of the input files',
-        type: 'string'
-    })
-    .option('l', {
-        alias: 'logLevel',
-        default: 'warning',
-        describe: 'Set the complier log level',
-        choices: ['debug', 'info', 'warning', 'error']
-    })
-    .options({
-        'swift': { 
-            group: 'Swift options:',
-            desc: 'Compile source to swift (enabled automatically if any swift option specified e.g. swift.outDir=gen)' 
-        },
-        'swift.javascriptcore': { 
-            group: 'Swift options:',
-            desc: 'Compile source to use the JavaScriptCore engine under the hood [default]',
-        },
-        'swift.bundleId': { 
-            group: 'Swift options:',
-            desc: 'The id of the bundle containing the javascript source file, omit to use the main bundle',
-            type: 'string'             
-        }
-    })
-    .options({ 
-        'outDir': { 
-            group: 'General options:',
-            desc: 'Redirect output structure to a directory',
-            type: 'string',
-            default: '.'             
-        },
-        'noEmit': { 
-            group: 'General options:',
-            desc: 'Do not emit complied output',
-            type: 'boolean',             
-            default: false             
-        },
-        'noEmitWrapper': { 
-            group: 'General options:',
-            desc: 'Do not emit the wrapper for the specified JS engine in compiled output',
-            type: 'boolean',         
-            default: false             
-        }
-    })
-    .epilog('General options can be applied globally or to any language or engine, e.g. swift.outDir or swift.javascriptcore.outDir')
-    .argv as any;
-    
+function main(...args: string[]): number {
 
-log.setLevel(options.logLevel);
+    let options = yargs
+        .usage('Usage: $0 [file.js] [options]')
+        .demand(1)
+        .check((argv: yargs.Argv, aliases: { [key: string]: string[] }) => {
+            return !argv._[0] || argv._[0].endsWith('.js') ? true : 'File argument must be a js source file (.js)';
+        })
+        .example('$0 src.min.js --swift', 'Compile to swift, outputting beside original source files')
+        .example('$0 src.js --java.outDir java', 'Compile to java, outputting to a java subdirectory')
+        .alias('v', 'version').version()
+        .help('h').alias('h', 'help')
+        .group(['p', 'l', 'h', 'v'], 'Global options:')
+        .option('p', {
+            config: true,
+            alias: 'project',
+            describe: 'Path to a xrails.json project config file (or to a directory containing one)',
+            type: 'string'
+        })
+        .option('charset', {
+            default: 'utf8',
+            describe: 'The character set of the input files',
+            type: 'string'
+        })
+        .option('l', {
+            alias: 'logLevel',
+            default: 'warning',
+            describe: 'Set the complier log level',
+            choices: ['debug', 'info', 'warning', 'error']
+        })
+        .options({
+            'swift': { 
+                group: 'Swift options:',
+                desc: 'Compile source to swift (enabled automatically if any swift option specified e.g. swift.outDir=gen)' 
+            },
+            'swift.javascriptcore': { 
+                group: 'Swift options:',
+                desc: 'Compile source to use the JavaScriptCore engine under the hood [default]',
+            },
+            'swift.bundleId': { 
+                group: 'Swift options:',
+                desc: 'The id of the bundle containing the javascript source file, omit to use the main bundle',
+                type: 'string'             
+            }
+        })
+        .options({ 
+            'outDir': { 
+                group: 'General options:',
+                desc: 'Redirect output structure to a directory',
+                type: 'string',
+                default: '.'             
+            },
+            'noEmit': { 
+                group: 'General options:',
+                desc: 'Do not emit complied output',
+                type: 'boolean',             
+                default: false             
+            },
+            'noEmitWrapper': { 
+                group: 'General options:',
+                desc: 'Do not emit the wrapper for the specified JS engine in compiled output',
+                type: 'boolean',         
+                default: false             
+            }
+        })
+        .epilog('General options can be applied globally or to any language or engine, e.g. swift.outDir or swift.javascriptcore.outDir')
+        .parse<CompilerOptions & {logLevel: string, charset: string}>(args);
 
-let compiler = new Compiler(options, [
-    [`swift`,   [`javascriptcore`]], 
-    ['java',    [`nashorn`, 'javascriptcore']],
-    [`csharp`,  [`chakracore`]], 
-    [`php`,     [`v8`]], 
-]);
+    log.setLevel(options.logLevel);
 
-let filename: string|undefined = args.argv._[0];
+    let compiler = new Compiler(options, [
+        [`swift`,   [`javascriptcore`]], 
+        ['java',    [`nashorn`, 'javascriptcore']],
+        [`csharp`,  [`chakracore`]], 
+        [`php`,     [`v8`]], 
+    ]);
 
-if(filename == undefined) {
-    log.debug('No filename supplied attempting to read package.json')
-    //todo
-} else {
-    process.exit(compiler.compile(new Module(filename, options.charset)));
-    // console.log(JSON.stringify(module, (key, value) => {
-    //     return value ? Object.assign(value, { kind: value.constructor.name }) : value;
-    // }, 4));       
+    let filename: string|undefined = options._[0];
+
+    if(filename == undefined) {
+        log.debug('No filename supplied attempting to read package.json')
+        //todo
+        return 1;
+    } else {
+        return compiler.compile(new Module(filename, options.charset));
+        // console.log(JSON.stringify(module, (key, value) => {
+        //     return value ? Object.assign(value, { kind: value.constructor.name }) : value;
+        // }, 4));       
+    }
 }
 
+export = main;
+
+if(require.main === module) {
+    let [,, ...args] = process.argv;
+    process.exit(main(...args));
+} else {
+    yargs.exitProcess(false);
+}
--- tests/js/mainSpec.ts
+++ tests/js/mainSpec.ts
@@ -4,27 +4,33 @@ describe("Main", () => {
     
     interface This {
         writeFileMethod: jasmine.Spy;            
-        exitMethod: jasmine.Spy;            
     }
     
     beforeEach(function(this: This) {
         this.writeFileMethod = spyOn(fs, 'writeFileSync');
-        this.exitMethod = spyOn(process, "exit");
     });
         
-    // it("fails when non js file arg given", function(this: This) {
-    //     process.argv = ['node', 'main', `types.ts`, "--swift"]
-    //     require('../../src/main');
-    //     expect(this.writeFileMethod).not.toHaveBeenCalled()
-    //     expect(this.exitMethod).toHaveBeenCalledWith(1);
-    // });
+    it("fails when non js file arg given", function(this: This) {
+        try {
+            require('../../src/main')(
+                `types.ts`, 
+                '--swift'
+            );
+            fail("did not fail");
+        } catch(e) {
+            expect(e).toMatch('must be a js source file')
+            expect(this.writeFileMethod).not.toHaveBeenCalled()
+        }
+    });
     
     it("successfully compiles the reference project", function(this: This) {
-        process.argv = ['node', 'main', `./tests/reference/types.js`, "--swift"]
-        require('../../src/main');
+        let exitCode = require('../../src/main')(
+            './tests/reference/types.js', 
+            '--swift'
+        );
+        expect(exitCode).toBe(0);
         expect(this.writeFileMethod).toHaveBeenCalledTimes(1)
         expect(this.writeFileMethod).toHaveBeenCalledWith(jasmine.stringMatching(/tests[\\\/]reference[\\\/]types\.swift/), jasmine.stringMatching(/optionalBooleanConst/));        
-        expect(this.exitMethod).toHaveBeenCalledWith(0);
     });
         
 });
