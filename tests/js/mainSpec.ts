2ec6502aeddb047b9c4fb9dcdf0ef27e597cdc43
Nick Williams <nbransby@gmaill.com>
2016-06-04 15:36:42 +0100

    more main tests

--- tests/js/mainSpec.ts
+++ tests/js/mainSpec.ts
@@ -9,7 +9,35 @@ describe("Main", () => {
     beforeEach(function(this: This) {
         this.writeFileMethod = spyOn(fs, 'writeFileSync');
     });
+            
+    it("successfully compiles the reference project", function(this: This) {
+        let exitCode = require('../../src/main')(
+            './tests/reference/types.js', 
+            '--swift'
+        );
+        expect(exitCode).toBe(0);
+        expect(this.writeFileMethod).toHaveBeenCalledTimes(1)
+        expect(this.writeFileMethod).toHaveBeenCalledWith(jasmine.stringMatching(/tests[\\\/]reference[\\\/]types\.swift/), jasmine.stringMatching(/optionalBooleanConst/));        
+    });
+
+    it("fails when no output languages given", function(this: This) {
+        let exitCode = require('../../src/main')(
+            './tests/reference/types.js' 
+        );
+        expect(exitCode).toBe(1);
+        expect(this.writeFileMethod).not.toHaveBeenCalled();
+    });
         
+    it("fails when no args given", function(this: This) {
+        try {
+            require('../../src/main')();
+            fail("did not fail");
+        } catch(e) {
+            expect(e).toMatch('need at least 1');
+            expect(this.writeFileMethod).not.toHaveBeenCalled();
+        }
+    });
+
     it("fails when non js file arg given", function(this: This) {
         try {
             require('../../src/main')(
@@ -22,15 +50,5 @@ describe("Main", () => {
             expect(this.writeFileMethod).not.toHaveBeenCalled()
         }
     });
-    
-    it("successfully compiles the reference project", function(this: This) {
-        let exitCode = require('../../src/main')(
-            './tests/reference/types.js', 
-            '--swift'
-        );
-        expect(exitCode).toBe(0);
-        expect(this.writeFileMethod).toHaveBeenCalledTimes(1)
-        expect(this.writeFileMethod).toHaveBeenCalledWith(jasmine.stringMatching(/tests[\\\/]reference[\\\/]types\.swift/), jasmine.stringMatching(/optionalBooleanConst/));        
-    });
-        
+
 });
