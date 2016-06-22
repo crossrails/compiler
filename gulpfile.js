6b2c6ae807e4aa7b4c92e24a485ed6c8d98cddf8
Nick Williams <nbransby@gmail.com>
2016-06-22 19:13:40 +0100

    rename reference to input and update scripts

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,14 @@ var concat = require('gulp-concat');
 var git = require('gulp-git');
 var del = require('del');
 
-gulp.task('swiftJSCWrapper', function() {
-  del(['temp']).then(paths => {
-    git.clone('https://github.com/Reedyuk/SwiftJSCWrapper.git', {args: 'temp'}, function (err) {
-      if (err) {
-        throw err;
-      } else {
-        gulp.src('temp/SwiftJSCWrapper/*.swift')
-          .pipe(concat('js.swift'))
-          .pipe(gulp.dest('tests/swift/src/'));
-      }
-    });
-  });
-  
-  
+gulp.task('javascriptcore.swift', function() {
+  git.clone('https://github.com/crossrails/javascriptcore.swift.git', {args: 'temp'}, function (err) {
+    if (err) {
+      throw err;
+    } else {
+      gulp.src('temp/Source/*.swift')
+        .pipe(concat('javascriptcore.swift'))
+        .pipe(gulp.dest('src/swift/src/'));
+    }
+  }).then(del(['temp']));
 });
