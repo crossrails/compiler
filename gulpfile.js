eaaecd083a10cd91de6777e616bdb3613884344d
Nick Williams <nbransby@gmail.com>
2016-06-24 19:56:16 +0100

    update gulp taskname

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,15 @@ var concat = require('gulp-concat');
 var git = require('gulp-git');
 var del = require('del');
 
-gulp.task('javascriptcore.swift', function() {
-  git.clone('https://github.com/crossrails/javascriptcore.swift.git', {args: 'temp'}, function (err) {
-    if (err) {
-      throw err;
-    } else {
+gulp.task('build-wrappers', function() {
+  del(['temp']).then(paths => {
+    git.clone('https://github.com/crossrails/javascriptcore.swift.git', {args: 'temp'}, (err) => {
+      if (err) {
+        throw err;
+      } 
       gulp.src('temp/Source/*.swift')
         .pipe(concat('javascriptcore.swift'))
-        .pipe(gulp.dest('src/swift/src/'));
-    }
-  }).then(del(['temp']));
+        .pipe(gulp.dest('src/swift/'));
+    })
+  });
 });
