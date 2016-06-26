c6edc3de6f5b116dc98a9bc48cd4b3fd064168c0
Andrew Reed <andrew_reed@hotmail.com>
2016-06-26 21:10:38 +0100

    Adjusted gulpfile to delete temp folder

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,15 @@ gulp.task('build-wrappers', function() {
     git.clone('https://github.com/crossrails/javascriptcore.swift.git', {args: 'temp'}, (err) => {
       if (err) {
         throw err;
+      } else {
+          gulp.src('temp/Source/*.swift')
+          .pipe(concat('javascriptcore.swift'))
+          .pipe(gulp.dest('src/swift/'))
+          .on('end', function() {
+            del(['temp']);
+          })
       } 
-      gulp.src('temp/Source/*.swift')
-        .pipe(concat('javascriptcore.swift'))
-        .pipe(gulp.dest('src/swift/'));
     })
   });
 });
+
