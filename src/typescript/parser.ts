c18d265d8b089c2343c5bf01138ef8398783ce6d
Nick Williams <nbransby@gmail.com>
2016-09-01 14:05:07 +0100

    refine visitor

--- src/typescript/parser.ts
+++ src/typescript/parser.ts
@@ -1,8 +1,9 @@
 import * as ts from "typescript";
 import {log} from "../log"
-import {Module, Comment, Declaration, Flags, SourceFile, NamespaceDeclaration, Type} from "../ast"
-import {walk, NodeVisitor, VariableDeclaration, FunctionDeclaration} from "./visitor"
+import {Module, Comment, Declaration, Flags, SourceFile, NamespaceDeclaration, ClassDeclaration, Type} from "../ast"
+import {visitNode, visitNodes, NodeVisitor, VariableDeclaration, FunctionDeclaration} from "./visitor"
 import {readFileSync} from 'fs';
+import {SymbolTable} from './symboltable'
 
 function getFlags(node: ts.Node): Flags {
     return (node.flags & ts.NodeFlags.Abstract) ? Flags.Abstract : Flags.None 
@@ -10,7 +11,7 @@ function getFlags(node: ts.Node): Flags {
         | (node.flags & ts.NodeFlags.Static) ? Flags.Static : Flags.None
 }
 
-export class Parser implements NodeVisitor<Declaration> {
+export class Parser {
 
     private readonly program: ts.Program;
     private readonly symbols: SymbolTable;
@@ -21,100 +22,35 @@ export class Parser implements NodeVisitor<Declaration> {
     }
 
     parse(sourceRoot: string): Module {
-        return new Module(sourceRoot, this.program.getSourceFiles().map(node => walk(node, this)[0] as SourceFile));
+        return new Module(sourceRoot, visitNodes(this.program.getSourceFiles(), this.createDeclarations, true, this) as SourceFile[]);
     }
 
-    shouldVisit(node: ts.Declaration): boolean {
-        if(this.symbols.isExported(node)) return true;
-        log.debug(`Skipping unexported ${ts.SyntaxKind[node.kind]}`, node);
-        return false;
+    private readonly createDeclarations: NodeVisitor<Declaration> = {
+
+        shouldVisitNode(this: Parser, node: ts.Declaration): boolean {
+            if(this.symbols.isExported(node)) return true;
+            log.debug(`Skipping unexported ${ts.SyntaxKind[node.kind]}`, node);
+            return false;
+        },
+
+        visitSourceFile(this: Parser, node: ts.SourceFile): Declaration {
+            // console.log(JSON.stringify(ts.createSourceFile(node.fileName, readFileSync(node.fileName).toString(), ts.ScriptTarget.ES6, false), (key, value) => { 
+            //     return value ? Object.assign(value, { kind: ts.SyntaxKind[value.kind], flags: ts.NodeFlags[value.flags] }) : value; 
+            // }, 4)); 
+            return new SourceFile(node.fileName, visitNode(node, this.createDeclarations, false));
+        },
+
+        visitNamespace(this: Parser, node: ts.ModuleDeclaration): Declaration {
+            return new NamespaceDeclaration(node.name!.text, getFlags(node), visitNodes(this.symbols.getExports(node), this.createDeclarations, false));           
+        },
+
+        visitClass(this: Parser, node: ts.ClassDeclaration): Declaration {
+            return new ClassDeclaration(node.name!.text, getFlags(node), visitNodes(this.symbols.getExports(node), this.createDeclarations, false), visitNodes(node.typeParameters!, this.createTypes));           
+        },
     }
 
-    visitSourceFile(node: ts.SourceFile): Declaration {
-        // console.log(JSON.stringify(ts.createSourceFile(node.fileName, readFileSync(node.fileName).toString(), ts.ScriptTarget.ES6, false), (key, value) => { 
-        //     return value ? Object.assign(value, { kind: ts.SyntaxKind[value.kind], flags: ts.NodeFlags[value.flags] }) : value; 
-        // }, 4)); 
-        return new SourceFile(node.fileName, walk(node, this, false));
-    }
+    private readonly createTypes: NodeVisitor<Type> = {
 
-    visitNamespace(node: ts.ModuleDeclaration): Declaration {
-        const nodes = this.symbols.getDeclarations<ts.ModuleDeclaration>(node, ts.SyntaxKind.ModuleDeclaration);
-        return new NamespaceDeclaration(node.name!.text, getFlags(node), 
-            nodes.reduce<Declaration[]>((reduced, node) => [...reduced, ...walk(node, this)], []));           
-    }
-}
-
-class SymbolTable implements NodeVisitor<boolean> {
-
-    private readonly checker: ts.TypeChecker;
-    private readonly implicitExport: boolean;
-    private readonly symbols = new Set<string>();
-    private readonly exports = new Map<ts.Declaration, ReadonlyArray<ts.Declaration>>();
-    private readonly thrown = new Set<string>();
-
-    constructor(program: ts.Program, implicitExport: boolean) {
-        this.checker = program.getTypeChecker();
-        this.implicitExport = implicitExport;
-        program.getSourceFiles().forEach(file => walk(file, this));
-    } 
-
-    isExported(node: ts.Declaration): boolean {
-        return this.exports.has(node);
-    }
-
-    getDeclarations<T extends ts.Declaration>(node: ts.Declaration, kind: ts.SyntaxKind): ReadonlyArray<T> {
-        return this.exports.get(node)!.filter(node => node.kind & kind) as T[];
-    }
-
-    private declarationsFor(declaration: ts.Declaration): ts.Declaration[] {
-        if(declaration.name === undefined) return [declaration];
-        const symbol = this.checker.getSymbolAtLocation(declaration.name);
-        return symbol == undefined ?  [declaration] : symbol.declarations!.filter( d => { 
-            switch(declaration.kind) {
-                case ts.SyntaxKind.FunctionDeclaration:
-                    return d.kind != ts.SyntaxKind.FunctionDeclaration || ((d as ts.FunctionDeclaration).parameters.length == (declaration as ts.FunctionDeclaration).parameters.length &&
-                        (d as ts.FunctionDeclaration).parameters.reduce((typesMatch, p, i) => typesMatch && p.type == (declaration as ts.FunctionDeclaration).parameters[i].type, true));
-                case ts.SyntaxKind.ModuleDeclaration:
-                    return d.kind != ts.SyntaxKind.FunctionDeclaration;
-            }
-            return true;
-        });        
-    }
-
-    private isMainDeclaration(node: ts.Node): boolean {
-        let declarations = this.declarationsFor(node as ts.Declaration);
-        return node === declarations.reduce((main, d) => !main || (main.kind == ts.SyntaxKind.ModuleDeclaration && d.kind != ts.SyntaxKind.ModuleDeclaration) ? d : main);
-    }
-
-    // visitNode(node ts.Node): boolean {
-    //     return 
-    // }
-
-    visitFunction(node: FunctionDeclaration): boolean {
-        const comment = new Comment(node);
-        comment.tagsNamed('throws').filter(tag => tag.type).forEach(tag => this.thrown.add(tag.type.name!));
-        return false;
-    }
-
-    visitIdentifier(node: ts.Identifier): boolean {
-        const symbol = this.checker.getSymbolAtLocation(node);
-        if(!(symbol.flags & ts.SymbolFlags.Export)) return true;
-        const name = this.checker.getFullyQualifiedName(symbol);
-        if(this.symbols.has(name)) return true;        
-        const comment = new Comment(node.parent!);
-        this.symbols.add(name);
-        const declaration = symbol.getDeclarations().reduce((main, d) => !main || (main.kind == ts.SyntaxKind.ModuleDeclaration && d.kind != ts.SyntaxKind.ModuleDeclaration) ? d : main); 
-        this.exports.set(declaration, symbol.getDeclarations());
-        //if type reference ensure type is exported
-        const type = this.checker.getTypeOfSymbolAtLocation(symbol, node);
-        const unexported = !this.symbols.has(this.checker.getFullyQualifiedName(type.getSymbol()));
-        if(unexported) type.getSymbol().getDeclarations().forEach((child: ts.Node | undefined) => {
-            for(let node = child; node; node = node.parent) {
-                node.flags |= ts.NodeFlags.Export;
-            }
-            walk(node.getSourceFile(), this);
-        });
-        return false;
     }
 }
 
--- src/typescript/visitor.ts
+++ src/typescript/visitor.ts
@@ -4,7 +4,7 @@ export type VariableDeclaration = ts.VariableDeclaration | ts.PropertyDeclaratio
 export type FunctionDeclaration = ts.FunctionDeclaration | ts.MethodDeclaration | ts.MethodSignature;
 
 export interface NodeVisitor<T> {
-    shouldVisit?(node: ts.Node): boolean
+    shouldVisitNode?(node: ts.Node): boolean
     visitSourceFile?(node: ts.SourceFile): T
     visitClass?(node: ts.ClassDeclaration): T
     visitInterface?(node: ts.InterfaceDeclaration): T
@@ -14,57 +14,61 @@ export interface NodeVisitor<T> {
     visitFunction?(node: FunctionDeclaration): T
     visitParameter?(node: ts.ParameterDeclaration): T
     visitIdentifier?(node: ts.Identifier): T
-    visitNode?(node: ts.Node): T
+    visitOtherNode?(node: ts.Node): T
 }
 
-export function walk<T>(node: ts.Node, visitor: NodeVisitor<T>, visitRoot: boolean = true, thisArg: any = visitor): T[] {
-    const visit = <N extends ts.Node>(visitMethod: ((node: N) => T) | undefined, node: N, visitChildren: (node: N) => T[] = (node) => []) => {
-        if(!visitRoot) return visitChildren(node);
-        if((visitMethod || visitor.visitNode) && visitor.shouldVisit && !visitor.shouldVisit(node)) return [];
-        if(!visitMethod) return visitor.visitNode ? Function.call(visitor.visitNode, thisArg, node) : [];
+export function visitNodes<T>(nodes: ReadonlyArray<ts.Node>, visitor: NodeVisitor<T>, visitRootNodes: boolean = true, thisArg: any = visitor): ReadonlyArray<T> {
+    return nodes.reduce<T[]>((reduced, node) => [...reduced, ...visitNode<T>(node, visitor, visitRootNodes, thisArg)], []);           
+}
+
+export function visitNode<T>(node: ts.Node, visitor: NodeVisitor<T>, visitRootNode: boolean = true, thisArg: any = visitor): ReadonlyArray<T> {
+    const visit = <N extends ts.Node>(visitMethod: ((node: N) => T) | undefined, node: N, visitChildren: (node: N) => ReadonlyArray<T> = (node) => []) => {
+        if(!visitRootNode) return visitChildren(node); 
+        if((visitMethod || visitor.visitOtherNode) && visitor.shouldVisitNode && !visitor.shouldVisitNode(node)) return [];
+        if(!visitMethod) return visitor.visitOtherNode ? Function.call(visitor.visitOtherNode, thisArg, node) : [];
         const result = Function.call(visitMethod, thisArg, node);
         return result ? [result] : visitChildren(node);
     }
     switch(node.kind) {
         case ts.SyntaxKind.SourceFile: 
             return visit(visitor.visitSourceFile, node as ts.SourceFile, (node) => {
-                return node.statements.reduce((t: T[], n: ts.Node) => [...t, ...walk(n, visitor)], [])                
+                return node.statements.reduce((t: T[], n: ts.Node) => [...t, ...visitNode(n, visitor)], [])                
             });        
         case ts.SyntaxKind.VariableStatement:
             const variables = (node as ts.VariableStatement).declarationList.declarations;
-            return variables.reduce((t: T[], n: ts.Node) => [...t, ...walk(n, visitor)], []);
+            return variables.reduce((t: T[], n: ts.Node) => [...t, ...visitNode(n, visitor)], []);
         case ts.SyntaxKind.VariableDeclaration:
         case ts.SyntaxKind.PropertyDeclaration:
         case ts.SyntaxKind.PropertySignature:
-            return visit(visitor.visitVariable, node as VariableDeclaration, (node) => walk(node.name, visitor));
+            return visit(visitor.visitVariable, node as VariableDeclaration, (node) => visitNode(node.name, visitor));
         case ts.SyntaxKind.FunctionDeclaration:
         case ts.SyntaxKind.MethodDeclaration:
         case ts.SyntaxKind.MethodSignature: 
             return visit(visitor.visitFunction, node as FunctionDeclaration, (node) => {
-                return walk(node.name!, visitor) || node.parameters.reduce((t: T[], n: ts.Node) => [...t, ...walk(n, visitor)], [])                
+                return visitNode(node.name!, visitor) || node.parameters.reduce((t: ReadonlyArray<T>, n: ts.Node) => [...t, ...visitNode(n, visitor)], [])                
             });        
         case ts.SyntaxKind.Constructor: 
             return visit(visitor.visitConstructor, node as ts.ConstructorDeclaration, (node) => {
-                return node.parameters.reduce((t: T[], n: ts.Node) => [...t, ...walk(n, visitor)], [])                
+                return node.parameters.reduce((t: T[], n: ts.Node) => [...t, ...visitNode(n, visitor)], [])                
             });        
         case ts.SyntaxKind.ClassDeclaration:
             return visit(visitor.visitClass, node as ts.ClassDeclaration, (node) => {
-                return walk(node.name!, visitor) || node.members.reduce((t: T[], n: ts.Node) => [...t, ...walk(n, visitor)], [])                
+                return visitNode(node.name!, visitor) || node.members.reduce((t: T[], n: ts.Node) => [...t, ...visitNode(n, visitor)], [])                
             });        
         case ts.SyntaxKind.InterfaceDeclaration:
             return visit(visitor.visitInterface, node as ts.InterfaceDeclaration, (node) => {
-                return walk(node.name, visitor) || node.members.reduce((t: T[], n: ts.Node) => [...t, ...walk(n, visitor)], [])                
+                return visitNode(node.name, visitor) || node.members.reduce((t: T[], n: ts.Node) => [...t, ...visitNode(n, visitor)], [])                
             });        
         case ts.SyntaxKind.ModuleDeclaration:
             return visit(visitor.visitNamespace, node as ts.ModuleDeclaration, (node) => {
                 const statements = (node.body as ts.ModuleBlock).statements;
-                return walk(node.name, visitor) || statements.reduce((t: T[], n: ts.Node) => [...t, ...walk(n, visitor)], [])                
+                return visitNode(node.name, visitor) || statements.reduce((t: T[], n: ts.Node) => [...t, ...visitNode(n, visitor)], [])                
             });        
         case ts.SyntaxKind.Parameter: 
-            return visit(visitor.visitParameter, node as ts.ParameterDeclaration, (node) => walk(node.name, visitor));
+            return visit(visitor.visitParameter, node as ts.ParameterDeclaration, (node) => visitNode(node.name, visitor));
         case ts.SyntaxKind.Identifier:
             return visit(visitor.visitIdentifier, node);
         default:
-            return visit(visitor.visitNode, node);
+            return visit(visitor.visitOtherNode, node);
     }    
 } 
