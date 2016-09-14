ba22bc8bccba0c41fd6cb598e74ad969323dacd3
Nick Williams <nbransby@gmail.com>
2016-09-14 22:02:56 +0100

    handle no path specified

--- src/main.ts
+++ src/main.ts
@@ -126,7 +126,7 @@ function main(...args: string[]): number {
 
     let filename: string|undefined = options._[0];
 
-    if (filename[0] === '~') {
+    if (filename && filename[0] === '~') {
         filename = path.join(process.env.HOME, filename.slice(1));
     }
     if(!filename || filename.endsWith('package.json')) {
